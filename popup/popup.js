87cc0cfd8bba819692b0c65bcbca00785f9d2473
gadfaria <gadfaria@gmail.com>
2022-12-19 11:15:13 -0300

    feat: Creating a logo

--- popup/popup.js
+++ popup/popup.js
@@ -20,12 +20,14 @@ const form = document.getElementById("form");
 
 form.addEventListener("submit", function (e) {
   // Send the query from the form to the background page.
-  const value = document.getElementById("input").value;
-  blockedSites = [...blockedSites, value];
+  const input = document.getElementById("input");
+  blockedSites = [...blockedSites, input.value];
   chrome.storage.sync.set({ blockedSites });
 
   let li = document.createElement("li");
-  li.innerText = value;
+  li.innerText = input.value;
+
+  input.value = "";
 
   // li.innerHTML += `<button class="remove">Remove</button>`;
   sites.appendChild(li);
@@ -35,18 +37,19 @@ form.addEventListener("submit", function (e) {
 
 form.addEventListener("reset", function (e) {
   chrome.storage.sync.set({ blockedSites: [] });
+  sites.innerHTML = "";
 });
 
-document.addEventListener("input", (e) => {
-  const el = e.target;
+// document.addEventListener("input", (e) => {
+//   const el = e.target;
 
-  if (el.matches("[data-color]")) {
-    clearTimeout(timer);
-    timer = setTimeout(() => {
-      document.documentElement.style.setProperty(
-        `--color-${el.dataset.color}`,
-        el.value
-      );
-    }, 100);
-  }
-});
+//   if (el.matches("[data-color]")) {
+//     clearTimeout(timer);
+//     timer = setTimeout(() => {
+//       document.documentElement.style.setProperty(
+//         `--color-${el.dataset.color}`,
+//         el.value
+//       );
+//     }, 100);
+//   }
+// });
